56a4094df2bfac0fec0313691c5894a335482a12
Georgy Alekseev <alekseevgeorgy@gmail.com>
2017-08-22 22:08:57 +0500

    fix/testing travis #2

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,31 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -ex
+npm install -g now
 
-# ENV Variables, Note: NOW_TOKEN in travisCI
-ORIGIN='*'
-# NOW config
-ALIAS='alekseev-burberry-fake-shop-for-study-dev.now.sh'
-
-export PATH="./node_modules/.bin:$PATH"
-
-# # 0. Use team
-# now switch $TEAM --token "$NOW_TOKEN"
-#
-# # 1. Wair for deployment ready
-URL=$(now -e ORIGIN="$ORIGIN" --public --token "$NOW_TOKEN")
-await-url "$URL"
-now ls --token "$NOW_TOKEN"
-
-# 2. Alias
-now alias set "$URL" "$ALIAS" --token "$NOW_TOKEN"
-
-# 3. Purge old services
-now remove --yes --safe --token "$NOW_TOKEN" $PROJECT
-
-# 4. Scale to 1
-now scale "$ALIAS" 1 --token "$NOW_TOKEN"
-
-# 5. Log results
-now ls --token "$NOW_TOKEN"
-now alias ls --token "$NOW_TOKEN"
+now deploy --token=$NOW_TOKEN
+now alias --token=$NOW_TOKEN
