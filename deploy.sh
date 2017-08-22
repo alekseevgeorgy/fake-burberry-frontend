fd7f46024f325d49fbcc54b7f3890ae6c29fcc7f
Georgy Alekseev <alekseevgeorgy@gmail.com>
2017-08-23 01:07:32 +0500

    fix/testing travis #10

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ALIAS='alekseev-burberry-fake-shop-for-study-dev.now.sh'
 URL=$(now build --public --token "$NOW_TOKEN")
-now -t "$NOW_TOKEN" rm -y "$ALIAS"
-now alias set "$URL" "$ALIAS" --token "$NOW_TOKEN"
+# now -t "$NOW_TOKEN" rm -y "$ALIAS"
+now alias "$URL" "$ALIAS" --token "$NOW_TOKEN"
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -87,7 +87,7 @@ export default () =>
     <Hamburger />
     <MediaQuery minWidth={768}>
       <CurrentStore>
-        Shopping in:&nbsp;<Span>United Kingdom biiiiitch (£)</Span>
+        Shopping in:&nbsp;<Span>United Kingdom fuuuuck (£)</Span>
       </CurrentStore>
     </MediaQuery>
     <Link to="/">
