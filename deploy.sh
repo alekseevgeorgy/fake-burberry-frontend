990a8d9880243c553d25d57ab4f916cbaa9f487e
Georgy Alekseev <alekseevgeorgy@gmail.com>
2017-08-23 00:15:16 +0500

    fix/testing travis #8

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 ALIAS='alekseev-burberry-fake-shop-for-study-dev.now.sh'
 URL=$(now build --public --token "$NOW_TOKEN")
-# await-url "$URL"
-# now ls --token "$NOW_TOKEN"
 now alias set "$URL" "$ALIAS" --token "$NOW_TOKEN"
-
-# now build --public --token=$NOW_TOKEN
-# now alias set $NOW_URL "alekseev-burberry-fake-shop-for-study-dev" --token=$NOW_TOKEN
